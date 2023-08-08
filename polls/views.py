07494f83a862c5d1fc3aa9fb5eb7a63cd7cbb7f3
suhailshaik98 <suhail1998@gmail.com>
2023-08-08 19:22:39 -0400

    Able to print a HTTP Response through app.py

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from polls import app
 
-# Create your views here.
+def home(request):
+    resultdict=app.weatherapi()
+    ans=resultdict["data"]
+    return HttpResponse(ans)
