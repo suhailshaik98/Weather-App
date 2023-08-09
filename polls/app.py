86c6848286f7f1fdc0c5f4d2c818e8f3ba4b9fec
Rajashekhar-Panuganti <shekarr52800@gmail.com>
2023-08-08 23:19:48 -0400

    Changing the code

--- polls/app.py
+++ polls/app.py
@@ -1,5 +1,5 @@
 def weatherapi():
     #write logic for getting the data from the api and returning the values inside a dictionary
     resultdict={}
-    resultdict["data"]="hello djnago lisbon"
+    resultdict["data"]="hello djnago amherst"
     return resultdict
