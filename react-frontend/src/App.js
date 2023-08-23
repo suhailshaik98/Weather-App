e87f6e1ce6384c1211ffabde360dfd2c98ddcd45
suhail <suhail1998@gmail.com>
2023-08-23 01:23:34 +0000

    Uploaded background and main layout

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Card, Container, Row, Col} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import sample from './bgvideo.mp4';
+
 
 // ---------------------------RESOURCES TO REFER---------------------------------------------------
 // Refer to this link https://react-bootstrap.netlify.app/docs/components/accordion
@@ -23,7 +25,7 @@ function App() {
         console.log(dataObject)
 
         const formattedTime = formatDateToISO8601Local(); // Define this function or replace with appropriate time formatting
-
+        console.log(formattedTime)
         let presenttime = -1;
 
         for (let i = 0; i < dataObject.length; i++) {
@@ -44,15 +46,39 @@ function App() {
         console.log(weathertimetable);
         let temperaturestring
         let timestring
-        for (let i = 0; i < weathertimetable.length; i++) {
-          temperaturestring += weathertimetable[i].temperature +" "
-          timestring += weathertimetable[i].time +" "
-        }
-        timestring=timestring.slice(9)
-        temperaturestring=temperaturestring.slice(9)
-        console.log(temperaturestring)
+      //   for (let i = 0; i < weathertimetable.length; i++) {
+      //     temperaturestring +=
+      //     weathertimetable[i].temperature +
+      // '  ' +
+      // '<div style="width: 2px;"></div> ';
+      //     timestring += weathertimetable[i].time +" "
+      //   }
+      const formatTime = (time) => {
+        const hour = parseInt(time.split(':')[0]);
+        const suffix = hour >= 12 ? 'am ' : 'pm';
+        const formattedHour = hour > 12 ? hour - 12 : hour;
+        return `${formattedHour} ${suffix}`;
+      };
 
-        setWeathertimetable(temperaturestring);
+      const weatherElements = weathertimetable.map((weatherData, i) => (
+          
+          <Col key={i}>
+          <Card   className="card border-0 bg-transparent">
+            <Card.Body>
+            <Card.Title style={{ fontSize:'90%' }}  >
+              {Math.round(weatherData.temperature)} C</Card.Title>
+              <Card.Text style={{ fontSize:'70%' }}>{formatTime(weatherData.time)}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+
+      ));
+        // timestring=timestring.slice(9)
+        // temperaturestring=temperaturestring.slice(9)
+        // console.log(temperaturestring)
+
+        setWeathertimetable(weatherElements);
         settime(timestring)
         setApiData(dataObject[dataObject.length - 1]); // Assuming you want to set the last item as apiData
       } catch (error) {
@@ -73,47 +99,74 @@ function App() {
   // }
 
   return (
+    
     <div className="App">
+          <video className='videoTag' autoPlay loop muted >
+    <source src={sample} type='video/mp4' />
+</video>
       <header className="App-header">
+
+
         <Container>
-          <Row>
-            <Col> </Col>
-            <Col>
-        <Card> 
-          <Card.Body>
-            <Card.Title>
-              Temperature
-            </Card.Title>
-            <Card.Text>
-              {apiData}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-            </Col>
-          </Row>
 
+         <Row>
+          <Col>
+          <div style={{ height: '40%' }}></div>
+          <Card className='card bg-transparent border-0'> 
+          <Card.Title style={{ fontSize:'160%' }}>Hello Buffalo!</Card.Title>
+          </Card>
+          </Col>
+          
+          <Col>
           <Row>
-          <Col> Hello </Col>
-          <Col>    </Col>
-          </Row>
-
-          <Row>
-            <Col> </Col>
-            <Col>
-        <Card> 
-          <Card.Body>
-            <Card.Title>
-              {timestring}
+          <Card className='card-glass'> 
+            <Container>
+              <Row>
+                <Col>
+                <Card className="card bg-transparent border-0">
+                  <Card.Body>
+                  <Card.Title><img src='https://www.blog.christinepolz.com/wp-content/uploads/2020/05/IMG_0920.gif' width="50%" height="50%"></img></Card.Title>
+                  </Card.Body>
+                </Card>
+                </Col>
+                <Col>
+                <div style={{ height: '20%' }}></div>
+                <Card className="card bg-transparent border-0">
+                <Card.Body>
+            <Card.Title style={{ fontSize:'160%' }}>
+              {Math.round(apiData)} C
             </Card.Title>
-            <Card.Text>
-            {temperaturestring}
-            </Card.Text>
           </Card.Body>
 
+</Card>
+                </Col>
+
+              </Row>
+
+            </Container>
+
         </Card>
-            </Col>
+
           </Row>
+          <div style={{ height: '10%' }}></div>
+          <Row>
+          <Card className="card-glass">
+                <Container>
+                  <Row>
+                  {temperaturestring}
+
+                  </Row>
+                </Container>
+
+              </Card>
+
+
+          </Row>
+          </Col>
+          </Row> 
+
         </Container>
+
       </header>
     </div>
   );
