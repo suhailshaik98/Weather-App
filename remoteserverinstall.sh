18f330da67e15a8605263263cdf8c183396c716e
Suhail Shaik <suhailmsu21@gmail.com>
2023-09-27 18:55:48 -0400

    Update remoteserverinstall.sh

--- remoteserverinstall.sh
+++ remoteserverinstall.sh
@@ -1,10 +1,6 @@
-echo "Removing existing zip files"
-sudo rm -rf cidcd.zip
 echo "Installing docker.io and docker compose"
 sudo apt install -y docker.io
 sudo apt-get install docker-compose-plugin
-echo "Loading the docker container"
-unzip cicd.zip
 sudo docker load -i weatherapp.tar
 echo "Stopping any instances of the docker"
 sudo docker compose down
