3ec776b8e34421a60146bf0243eb56a84baa37b9
Suhail Shaik <suhailmsu21@gmail.com>
2023-09-27 19:37:21 -0400

    Update remoteserverinstall.sh

--- remoteserverinstall.sh
+++ remoteserverinstall.sh
@@ -1,8 +1,8 @@
 echo "Installing docker.io and docker compose"
 sudo apt install -y docker.io
-sudo apt-get install docker-compose-plugin
+sudo apt  install docker-compose
 sudo docker load -i weatherapp.tar
 echo "Stopping any instances of the docker"
-sudo docker compose down
+cd dockercontainer && sudo docker-compose down
 echo "Making the docker compose up"
-docker compose up
+cd dockercontainer && sudo docker-compose up
